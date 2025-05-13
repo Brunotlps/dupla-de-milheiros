1f4e229078f8ad3638d9f767f8daea2c4ab0bbdf
bruno <brunoteixlps@gmail.com>
2025-05-12 22:51:00 -0300

    5° commit - Implementando a tela de login, necessario melhorar a UI

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('news/', include('news.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
