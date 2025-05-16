2c0efcf1effccdc898843e303cd4f63889258374
bruno <brunoteixlps@gmail.com>
2025-05-15 22:01:54 -0300

    7° commit - Foi revisada e melhorada toda a parte de sessions do site

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', include('home.urls')),
     path('news/', include('news.urls')),
     path('accounts/signup/', core_views.signup_view, name='signup'),
+    path("accounts/login/", core_views.CustomLoginView.as_view(
+        template_name="registration/login.html"
+    ), name="login"),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
--- core/views.py
+++ core/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib import messages
 from .forms import CustomUserCreationForm
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.views import LoginView
+
+class CustomLoginView(LoginView):
+
+    def dispatch(self, request, *args, **kwargs):
+        
+        if request.user.is_authenticated:
+            messages.info(request, "Você já está conectado.")
+            return redirect('home')
+        return super().dispatch(request, *args, **kwargs)
+
 
 def signup_view(request):
+
+    if request.user.is_authenticated:
+        messages.info(request, "Você já possui uma conta!")
+        return redirect('home')
+
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -17,3 +34,8 @@ def signup_view(request):
         form = CustomUserCreationForm()
     
     return render(request, 'registration/signup.html', {'form': form})
+
+def custom_logout_view(request):
+    auth_logout(request)
+    messages.info(request, "Logout realizado com sucesso!")
+    return redirect('home')
